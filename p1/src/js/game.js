c10a0a7a6ddd076e7e566e858487c7c342651a96
Alan Rawkins <arawkins@gmail.com>
2014-01-24 23:23:55 -0800

    Added smooth cube movement

--- p1/src/js/game.js
+++ p1/src/js/game.js
@@ -24,6 +24,9 @@
 	  
 	  this.game.world.setBounds(0, 0, worldWidth, worldHeight);
       player = this.add.sprite(0, 0, 'player');
+	  player.xPos = 0;
+	  player.yPos = 0;
+	  
 	  this.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON);
 	  this.camera.bounds = null;
 	  
@@ -47,7 +50,36 @@
     update: function () {
 		//this.camera.x = player.x - this.camera.width / 2;
 		//this.camera.y = player.y - this.camera.height /2 ;
-		
+		if (player.targetY != null) {
+			if (player.targetY > player.y) {
+				player.y += moveSpeed;
+				if (player.y >= player.targetY) {
+					player.y = player.targetY;
+					player.targetY = null;
+				} 
+			} else if (player.targetY < player.y) {
+				player.y -= moveSpeed;
+				if (player.y <= player.targetY) {
+					player.y = player.targetY;
+					player.targetY = null;
+				}
+			}
+			
+		} else if (player.targetX != null) {
+			if (player.targetX > player.x) {
+				player.x += moveSpeed;
+				if (player.x >= player.targetX) {
+					player.x = player.targetX;
+					player.targetX = null;
+				} 
+			} else if (player.targetX < player.x) {
+				player.x -= moveSpeed;
+				if (player.x <= player.targetX) {
+					player.x = player.targetX;
+					player.targetX = null;
+				}
+			}
+		}
     },
 
     onMouseUp: function (event) {
@@ -56,16 +88,19 @@
 		var ang =  Math.atan2(dy,dx) * (180/Math.PI);
 		if (ang < 0) ang = 360 + ang;
 		if(ang < 45 || ang >= 315) {
-			player.vx = moveSpeed;
-			
+			player.targetX = player.x + cubeWidth;
 		} else if (ang >= 45 && ang < 135) {
-			player.vy = moveSpeed
+			player.targetY = player.y + cubeHeight;
 		} else if (ang >= 135 && ang < 225) {
-			player.vx = -moveSpeed;
+			player.targetX = player.x - cubeWidth;
 		} else if (ang >= 225 && ang < 315) {
-			player.vy = -moveSpeed;
+			player.targetY = player.y - cubeHeight;
 		}
-    }
+    },
+	
+	addNPC: function() {
+		var npc = this.add.sprite(0, 0, 'player');
+	}
 
   };
 
