c2f27d68af8116132f2011991eb7b69a90119008
xer0x <drew666@gmail.com>
2014-01-26 08:52:20 -0800

    remove consolelog

--- p1/src/js/game.js
+++ p1/src/js/game.js
@@ -214,7 +214,7 @@
 
 		var timeClock = function() {
 			timeRemaining--;
-			console.log(timeRemaining + " seconds left");
+			//console.log(timeRemaining + " seconds left");
 			if (timeRemaining < 10 && timeRemaining >= 0) {
 				timerText.innerHTML = ":0" + timeRemaining;
 			} else {
