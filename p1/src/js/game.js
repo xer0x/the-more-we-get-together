bee4f1540f318531a9bc7dceb0dc8c00e4cb9c32
Luke Pacholski <luke.pacholski@gmail.com>
2014-01-26 01:43:30 -0800

    Timer updates

--- p1/src/js/game.js
+++ p1/src/js/game.js
@@ -231,7 +231,12 @@
 				timeRemaining--;
 
 				//console.log(timeRemaining + " seconds left");
-				timerText.innerHTML = ":" + timeRemaining;
+				if(timeRemaining < 10) {
+				  timerText.innerHTML = ":0" + timeRemaining;
+				} else {
+          timerText.innerHTML = ":" + timeRemaining;
+				}
+
 				if(timeRemaining <= 0) {
 					started = false;
 					ended = true;
